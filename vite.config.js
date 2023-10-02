b8f3ece1d02ef0d6b5a10458850381594829ca1f
omell22 <dima22sw03@gmail.com>
2023-10-02 10:05:17 +0300

    123

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/wedding/',
    plugins: [vue()],
    modules: [
       '@swiper/pagination',
